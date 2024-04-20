2cbc66f37d820c32dfd3d9bdc9ff61278309f12b
Axis <erlia@naver.com>
2024-04-21 04:35:22 +0900

    1 2 3

--- src/Test6_22.java
+++ src/Test6_22.java
@@ -1,12 +1,12 @@
 public class Test6_22 {
 
     static boolean isNumber(String str) {
-           //여기 확인   지역변수이면 초기화를 해줘야 하나? 해줘야 한다..
+           //여기 확인   1.지역변수이면 초기화를 해줘야 하나? 해줘야 한다..
 
         if (str==null || str.equals(""))             return false;
-           //객체 참조변수가 아무것도 가리키지 않을 때 null을 사용, 기본자료형에서는 쓰일수 없음..
-            for (int i = 0; i < str.length(); i++){
-                  char ch= str.charAt(i); //지정한 위차의 문자를 반환
+           //2.객체 참조변수가 아무것도 가리키지 않을 때 null을 사용, 기본자료형에서는 쓰일수 없음..
+            for (int i = 0; i < str.length(); i++){      //3.배열의 크기는 length, 문자열의 크기는 length()
+                  char ch= str.charAt(i);               //4.지정한 위차의 문자를 반환
                   if(ch<'0' || ch>'9') return false;
                }
     return  true;
