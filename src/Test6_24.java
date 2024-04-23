4a72908fd00dee805b9498369426ccc14d9f57be
Axis <erlia@naver.com>
2024-04-23 19:01:29 +0900

    1 2 3

--- src/Test6_24.java
+++ src/Test6_24.java
@@ -1,13 +1,13 @@
 public class Test6_24 {
 
     static int abs(int value) {
-        int a = value;
+
         if (value < 0) {
 
-            a = (value * -1);
+            return value * -1;
 
         }
-        return a;
+        return value;
     }
     public static void main(String[] args) {
         int value=5;
